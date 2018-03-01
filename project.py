c17ece38fcd943f06b8811ad98e8db0d6991ce52
klita <kejti.lita@intel.com>
2018-03-01 08:50:58 -0800

    fd

--- project.py
+++ project.py
@@ -1,9 +1,9 @@
 print 11
-number = 2
-
-print number
-#printtt number
-print wrong (3,2)
-def wrong (a, b):
-    c = a +b
-    return c
+number = 23
+#
+# print number
+# #printtt number
+# print wrong (2,2)
+# def wrong (a, b):
+#     c = a +b
+#     return c
