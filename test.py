add845144ef9165550b026d35f721d2ec1ecccbd
klita <kejti.lita@intel.com>
2018-03-15 23:47:21 -0700

    Modifications to test releasenotes.py

--- test.py
+++ test.py
@@ -23,14 +23,12 @@ args = vars(parser.parse_args())
 repodir = r'.'
 print "This is the repodir" + repodir
 #username = username.replace("'", "")
-#password = password.replace("'","")
-#print username
+ #print username
 #print password
 #hgpath = r"C:\Program Files\TortoiseHg\hg.exe" # PATH TO HG.EXE
 
 #repoURL = "https://" + username + ':' + password + "@nsg-hg.intel.com/index.py/SVC_SQE_Firmware_Test/"
 #repoURL = "https://" + username + ':' + password + "@nsg-hg.intel.com/index.py/TWIDL_Trunk"
-
 
 if not os.path.exists(repodir):
     print 'Whoops, invalid local directory for repository.'
@@ -56,6 +54,9 @@ if not os.path.exists(repodir):
 #print cout, cerr
 #if hg.returncode != 0:
 #    sys.exit(1)
+
+
+"""I havent made any changes here"""
 def create_revision(repodir):
     f = open(repodir + r'\kbase\release.txt', 'r+')
     firstLine = f.readline().strip().split()
@@ -90,24 +91,25 @@ def create_revision(repodir):
     f.seek(0)
     f.write(' '.join(firstLine) + '\n')
     f.close()
-
+"""made this to get the commit message"""
 def commit_msg(repodir):
     f = open(repodir + r'\kbase\release.txt', 'r+')
     firstLine = f.readline().strip().split()
     firstLine[2] = version
     print version
     commitMsg= "Update Test environment with TWIDL " + version
-
     return commitMsg
 
 
 
 #autocodepromote.AutoCodePromote.promote_change( )
 
-
+"""Calling Nate's code"""
 a = AutoCodePromote(r"C:\test", disable_lfs=True, promote_retry_count=3)
 commit_hash = a.promote_change(create_revision(repodir), "klita_test", commit_msg(repodir), tmp_banch_prefix="tmp", ff_ancestor_branch='master')
 
+
+""""Adapted to git fron hg"""
 def create_releasenotes():
     gitpath ='C:\Program Files\Git\mingw64\\bin\git.exe'
     repodir = r'.'
@@ -116,17 +118,16 @@ def create_releasenotes():
                               stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=repodir)
     cout, cerr = git.communicate()
     git.wait()
-    print cout, "is cout"
-
+    print cout
     notes = cout.strip("\"\"").split('\n')
-    print notes , 'is notes'
+    print notes
 
     if git.returncode != 0:
         sys.exit(1)
-    # f = open(r'Y:\fast-tools-sqe\rev.txt', 'r+')
+    #f = open(r'Y:\fast-tools-sqe\rev.txt', 'r+')
     f = open(r'C:\Users\klita\Documents\twidlgit\rev1.txt', 'r+')
     old = f.readline().strip()
-    print old , "is old "
+    print old
     f.seek(0)
     f.write(cout.strip("\""))
     f.close()
@@ -134,6 +135,7 @@ def create_releasenotes():
     diff_revisions = old.strip("\"") + '^..' + cout.strip().strip("\"")
     #diff_revisions = '460e9412fd54deb6ae54e014897f3e2f13318546' +'a6e27f64d1de2b67cfada1ba9b0f2448c99bb365'
     print 'Storing all commits between', diff_revisions
+    """Shows the commit message and the commit hash"""
     git = subprocess.Popen(['git', 'log', '-M', '-r','--pretty=oneline', diff_revisions],
                               stdin=subprocess.PIPE,
                               stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=repodir)
@@ -143,6 +145,8 @@ def create_releasenotes():
     if git.returncode != 0:
         if ('no changes found' not in cout):
             sys.exit(1)
+
+    
     notes = cout.strip().split('\n')[0:]
     #f = open(r'Y:\fast-tools-sqe\ReleaseNotes.txt', 'w+')
     f = open(r'C:\Users\klita\Documents\twidlgit\ReleaseNotes.txt', 'w+')
